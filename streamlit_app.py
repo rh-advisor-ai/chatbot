9a7882b0049fe7ec355d642614a9f64a813c81f3
rh-advisor-ai <rh.advisor.ai@gmail.com>
2024-09-23 15:12:45 +0000

    fix7

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ st.title("💬 Chatbot")
 st.write(
     "This is a simple chatbot that uses OpenAI's"
 )
+# st.info(f"user name: {st.session_state.get('user_name')}")
+# st.info(f"user ID: {st.session_state.get('user_id')}")
 
 # option = st.selectbox(
 #     "How would you like to be contacted?",
@@ -18,20 +20,57 @@ st.write(
 # )
 # st.write("You selected:", option)
 
-if "username" not in st.session_state:
-    username = st.text_input("Username", type="default", key="username")
-    user_id = randint(1000, 10000)
-    # st.session_state.username = username
-    st.session_state.user_id = user_id
-    st.rerun()
-    input_json = {
-        "userId" : st.session_state.user_id
-    }
-    requests.post(f"{base_url}/start_thread", json=input_json).json()
-else:
-    st.text("Hello " + st.session_state.username)
+def start_thread_ai():
+    if st.session_state.username and st.session_state.username != '':
+        user_id = randint(1000, 10000)
+        print(f"creating user ID: {str(user_id)}")
+        st.session_state.user_name = st.session_state.username
+        st.session_state.user_id = user_id
+        # st.rerun()
+        input_json = {
+            "userId" : st.session_state.user_id
+        }
+        requests.post(f"{base_url}/start_thread", json=input_json).json()
 
-if not st.session_state.username:
+def exit_chat():
+    st.session_state.messages = []
+    st.session_state.user_id = None
+    del st.session_state['user_name']
+    del st.session_state['user_id']
+    # st.sidebar.empty()
+    # st.sidebar.status(expanded=False)
+    # del st.sidebar
+    # st.rerun()
+
+def run_chat():
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": st.session_state.prompt})
+    # with st.chat_message("user"):
+    #     st.markdown(st.session_state.prompt)
+    input_json = {
+        "userId" : st.session_state.user_id,
+        "question" : st.session_state.prompt
+    }
+    data = requests.post(f"{base_url}/chat_advisor", json=input_json).json()
+
+    # Stream the response to the chat using `st.write_stream`, then store it in 
+    # session state.
+    # with st.chat_message("assistant"):
+    #     st.markdown(data['answer'])
+    st.session_state.messages.append({"role": "assistant", "content": data['answer']})
+    st.session_state.prompt = None
+
+
+def phase_two():
+    print()
+    st.session_state.messages = []
+
+if "user_name" not in st.session_state or not st.session_state.user_name:
+    username = st.text_input("Username", type="default", key="username", on_change=start_thread_ai)
+else:
+    st.text("Hello " + st.session_state.user_name)
+
+if "user_name" not in st.session_state or not st.session_state.user_name:
     st.info("Please add your Username to continue.", icon="🗝️")
 else:
     # Create a session state variable to store the chat messages. This ensures that the
@@ -44,28 +83,21 @@ else:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if st.sidebar.button("Next Steps", key='clear_chat_button'):
-            st.session_state.messages = []
-            # move_focus()
+    # if st.sidebar.button("Next Steps", key='next_chat_button'):
+    #     st.session_state.messages = []
+    #     # move_focus()
+
+    # st.sidebar.button("Exit", key='clear_chat_button', on_click=exit_chat)
+        
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        input_json = {
-            "userId" : st.session_state.user_id,
-            "question" : prompt
-        }
-        data = requests.post(f"{base_url}/chat_advisor", json=input_json).json()
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            st.markdown(data)
-        st.session_state.messages.append({"role": "assistant", "content": data['answer']})
+    # st.chat_input("What is up?", key="prompt", on_submit=run_chat)
+    st.text_input("Chat", type="default", key="prompt", on_change=run_chat)
     
-    # st.button("Next Step")
+    col1, col2 = st.columns([1,1])  # Adjust column ratios as needed
+    with col1:
+        st.button("Exit", key='clear_chat_button', on_click=exit_chat)
+
+    with col2:
+        st.button("Next Step", key='next_chat_button', on_click=exit_chat)
